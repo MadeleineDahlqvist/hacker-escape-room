de62f138101481f8f95ff334ac5fc582a743862d
Madeleine <madeleine.dahlqvist@lerniastudent.se>
2024-03-08 20:57:33 +0100

    Add test for booking

--- cypress/e2e/test-localhost.cy.js
+++ cypress/e2e/test-localhost.cy.js
@@ -1,5 +1,14 @@
 describe('See if page is up and running on Localhost', () => {
     it('sees if page is running on Localhost', () => {
       cy.visit('127.0.0.1:5500');
-    })
-  })
+    });
+  });
+
+  describe('See if booking modal is working', () => {
+    it('See if booking modal is working', () => {
+      cy.visit('https://madeleinedahlqvist.github.io/hacker-escape-room/');
+      cy.get('.rooms__button').first().click();
+      cy.get('.modal__contentWrapper').should('be.visible');
+      cy.get('.modal__date').click().type('2024-03-03');
+    });
+  });
